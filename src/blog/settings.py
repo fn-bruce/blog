e64fb6b454bbe2d8b172de18fce1d375ee691ccd
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2020-08-27 20:37:28 -0600

    rotated secret key and only retrieve secret key from an environmental variable

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '81qvy=ru19%)ng^ev55q&*elkp1kpqp#yb-*$y--2=fuq(&3d0'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
