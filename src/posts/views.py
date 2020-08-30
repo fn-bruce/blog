9cf302769aa23313f45cd3da4dde5b4087dfe900
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2020-08-30 12:52:06 -0600

    can grab most recent posts

--- src/posts/views.py
+++ src/posts/views.py
@@ -22,6 +22,7 @@ def index(request):
 
 
 def blog(request):
+    most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 4)
     page_request_var = 'page'
@@ -36,10 +37,11 @@ def blog(request):
 
     context = {
             'queryset': paginated_queryset,
+            'most_recent': most_recent,
             'page_request_var': page_request_var
             }
     return render(request, 'blog.html', context)
 
 
-def post(request):
+def post(request, id):
     return render(request, 'post.html', {})
