7d64a1d1b4b47f101d7640887bc8874080ddc8a7
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2020-08-30 17:32:39 -0600

    implemented the contents of a post using TinyMCE

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from posts.views import index, blog, post, search
 
@@ -26,8 +26,11 @@ urlpatterns = [
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
     path('post/<id>/', post, name='post-detail'),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+            settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+            settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
